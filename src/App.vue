4c5b41b612f37dcb1072a84e7ceef67fe5f51afe
pkzala <pratiksha.pysquad@gmail.com>
2022-08-29 12:33:34 +0530

    feat: flow chart.

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="app">
+        <button type="button" @click="$refs.chart.add({id: +new Date(), x: 10, y: 10, name: '', type: 'terminal', approvers:[{name: 'admin'}]})">
+            Add Element
+        </button>
+        <button type="button" @click="$refs.chart.remove()">
+            Delete element
+        </button>
+        <flowchart
+           @dblclick="handleDblClick"
+           ref="chart">
+        </flowchart>
+    </div>
+</template>
+<script>
+  import Vue from 'vue';
+  import FlowChart from 'flowchart-vue';
+
+  Vue.use(FlowChart);
+
+  export default {
+    name: 'App',
+
+    methods: {
+      handleDblClick(position) {
+        this.$refs.chart.add({
+          id: new Date(),
+          x: position.x,
+          y: position.y,
+          name: 'New',
+          type: 'start',
+          approvers: [],
+        });
+      },
+    }
+  };
+</script>
